64bb72a61992df7978b8ffb7d7ef9f2674723a31
Dmitry1964 <nosdm64@gmail.com>
2025-08-25 20:37:54 +0300

    Добавляет logo & search field в navbar

--- client/src/widgest/navbar/ui/navbar.tsx
+++ client/src/widgest/navbar/ui/navbar.tsx
@@ -6,9 +6,14 @@ const Navbar = () => {
       <div className={cls.navbar__wrapper}>
         <div className={cls.navbar__logo}>
           <img src="content/svg/logo.svg" width={148} height={28} alt="Логотип дашбоард" />
+          <button className={cls.navbar__btn}></button>
+        </div>
+        <div className={cls.navbar__search}>
+          <img className={cls.navbar__search_img} src="content/svg/icon-search.svg" width={16} height={16} alt="Иконка-поиск" />
+          <input className={cls.navbar__search_field} type="text" placeholder='Search for...' />
         </div>
       </div>
-      
+
     </nav>
   )
 }
